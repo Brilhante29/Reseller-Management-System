0fc94e15b24a6045afef1d5833269e534bde902f
Brilhante29 <guilhermebrilhante00@gmail.com>
2024-07-14 03:48:33 -0300

    fix: fix swagger documentation

--- src/main/java/com/br/mobiauto/configs/SwaggerConfig.java
+++ src/main/java/com/br/mobiauto/configs/SwaggerConfig.java
@@ -1,61 +1,39 @@
 package com.br.mobiauto.configs;
 
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class SwaggerConfig {
+
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("Mobiauto Backend")
-                        .description("API responsável pela gestão de Revendas de veículos")
+                        .title("Reseller Backend")
+                        .description("This backend system is responsible for the management of dealerships, providing tools for user, dealership, and business opportunity management, ensuring a secure and efficient process for buying and selling vehicles.")
                         .version("1"))
-                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
-                .components(new io.swagger.v3.oas.models.Components()
-                        .addSecuritySchemes("bearerAuth",
-                                new SecurityScheme()
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")));
+                .schemaRequirement("jwt_auth", creaSecurityScheme());
     }
 
     @Bean
-    public GroupedOpenApi authApi() {
+    public GroupedOpenApi customApi() {
         return GroupedOpenApi.builder()
-                .group("auth")
-                .pathsToMatch("/api/auth/**")
+                .group("default")
+                .pathsToMatch(
+                        "/api/dealerships/**",
+                        "/api/users/**",
+                        "/api/opportunities/**",
+                        "/api/auth/**"
+                )
                 .build();
     }
 
-    @Bean
-    public GroupedOpenApi userApi() {
-        return GroupedOpenApi.builder()
-                .group("users")
-                .pathsToMatch("/api/users/**")
-                .build();
-    }
-
-    @Bean
-    public GroupedOpenApi dealershipsApi() {
-        return GroupedOpenApi.builder()
-                .group("dealerships")
-                .pathsToMatch("/api/dealerships/**")
-                .build();
-    }
-
-    @Bean
-    public GroupedOpenApi opportunitiesApi() {
-        return GroupedOpenApi.builder()
-                .group("opportunities")
-                .pathsToMatch("/api/opportunities/**")
-                .build();
+    private SecurityScheme creaSecurityScheme() {
+        return new SecurityScheme().name("jwt_auth").type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT");
     }
 }
